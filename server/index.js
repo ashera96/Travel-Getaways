2c8140d2a6cdf325e29f094dea1859ace086b941
marioperera <pereramario19@gmail.com>
2019-04-26 19:25:15 +0530

    server

--- server/index.js
+++ server/index.js
@@ -1,22 +1,44 @@
 var express = require("express");
-const MongoClient = require("mongodb");
+//const MongoClient = require("mongodb");
+//var mongoose = require(__dirname + "/mlab.js");
 var app = express();
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
 
 const uri =
-  "mongodb+srv://admin:marper96@cluster0-dtohb.mongodb.net/test?retryWrites=true";
+  "mongodb+srv://admin:marper96@cluster0-dtohb.gcp.mongodb.net/test?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
-app.post("/GetUsers", function(_req, res) {
-  MongoClient.connect(uri, { useNewUrlParser: true }, (_err, database) => {
-    const myAwesomeDB = database.db("travelGetaway");
-    var users = myAwesomeDB.collection("users");
-    users.find({}, (err, _result) => {
-      if (err) throw err;
-
-      res.send(_result);
+app.post("/GetUsers", (req, res) => {
+  client.connect(err => {
+    const collection = client.db("travelGetaway").collection("users");
+    // perform actions on the collection object
+    collection.find({}).toArray(function(err, posts) {
+      assert.equal(err, null, "no post object present in line 40");
+      console.log("found following users");
+      console.log(posts);
+      res.send(posts);
     });
+
+    client.close();
   });
 });
+
+app.post("/GetPosts", (req, res) => {
+  client.connect(err => {
+    const collection = client.db("travelGetaway").collection("posts");
+    // perform actions on the collection object
+    collection.find({}).toArray(function(err, posts) {
+      assert.equal(err, null, "no post object present in line 40");
+      console.log("found following users");
+      console.log(posts);
+      res.send(posts);
+    });
+
+    client.close();
+  });
+});
+
 app.listen(3000, function() {
   console.log("server listning on port 3000");
 });
