a6f488b7b50d573bdb87d44012c054682c961db0
Nisal Bandara <nisaldbandara96@gmail.com>
2019-04-29 20:37:23 +0530

    ignore env

--- server/index.js
+++ server/index.js
@@ -5,8 +5,7 @@ var app = express();
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
-const uri =
-  "mongodb+srv://admin:marper96@cluster0-dtohb.gcp.mongodb.net/test?retryWrites=true";
+const uri = "<<Mongo URI>>";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 app.post("/GetUsers", (req, res) => {
