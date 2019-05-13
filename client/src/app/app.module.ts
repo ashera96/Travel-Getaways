68e0a2606ed6595cbf6a497a0ab64da0503eef9e
marioperera <pereramario19@gmail.com>
2019-05-13 23:37:10 +0530

    chatbot added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { HomeContentComponent } from './pages/home/home-content/home-content.component';
-import { AboutComponent } from './pages/about/about.component';
-import { AboutContentComponent } from './pages/about/about-content/about-content.component';
-import { ToursComponent } from './pages/tours/tours.component';
-import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { ContactContentComponent } from './pages/contact/contact-content/contact-content.component';
-import { TestimonialsContentComponent } from './pages/testimonials/testimonials-content/testimonials-content.component';
-import { ChatBotComponent } from './chat-bot/chat-bot.component';
+import { AppComponent } from "./app.component";
+import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+import { HomeContentComponent } from "./pages/home/home-content/home-content.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { AboutContentComponent } from "./pages/about/about-content/about-content.component";
+import { ToursComponent } from "./pages/tours/tours.component";
+import { TestimonialsComponent } from "./pages/testimonials/testimonials.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { RegisterComponent } from "./pages/register/register.component";
+import { ContactContentComponent } from "./pages/contact/contact-content/contact-content.component";
+import { TestimonialsContentComponent } from "./pages/testimonials/testimonials-content/testimonials-content.component";
+import { ChatBotComponent } from "./chat-bot/chat-bot.component";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -39,10 +42,14 @@ import { ChatBotComponent } from './chat-bot/chat-bot.component';
     ChatBotComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- client/src/app/chat-bot/chat-bot.component.ts
+++ client/src/app/chat-bot/chat-bot.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { element } from "@angular/core/src/render3";
+import { FormControl, Validators } from "@angular/forms";
+import { HttpService } from "../http.service";
 
 @Component({
   selector: "app-chat-bot",
@@ -10,8 +12,10 @@ export class ChatBotComponent implements OnInit {
   public toggle: boolean = false;
   @ViewChild("pagecontainer") pagecontainerelement: ElementRef;
   public pagecontainer: HTMLElement;
+  public input = new FormControl("", [Validators.required]);
+  public chatList = new Array();
 
-  constructor() {}
+  constructor(private httpService: HttpService) {}
 
   ngOnInit() {}
 
@@ -22,13 +26,25 @@ export class ChatBotComponent implements OnInit {
       this.toggle = false;
       this.pagecontainer = <HTMLElement>this.pagecontainerelement.nativeElement;
       this.pagecontainer.classList.toggle("hide");
-      console.log("hide");
+      //console.log("hide");
     } else {
       this.toggle = true;
-      console.log("hide");
+      //console.log("hide");
 
       this.pagecontainer = <HTMLElement>this.pagecontainerelement.nativeElement;
       this.pagecontainer.classList.toggle("expand");
     }
+  }
+
+  getReply() {
+    let sendObj = {
+      id: "user",
+      val: this.input.value
+    };
+    this.chatList.push(sendObj);
+    this.httpService.realizarHttpPost("URL", sendObj).subscribe(res => {
+      let resObj = { id: "bot", val: res };
+      this.chatList.push(resObj);
+    });
   }
 }
