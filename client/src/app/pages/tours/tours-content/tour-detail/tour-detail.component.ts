282f13b5c52e5245fda4eb8dc39f4621ba8acf34
Ashera Silva <asherasilva100@gmail.com>
2019-05-27 06:35:42 +0530

    changes in boomarking component

--- client/src/app/pages/tours/tours-content/tour-detail/tour-detail.component.ts
+++ client/src/app/pages/tours/tours-content/tour-detail/tour-detail.component.ts
@@ -15,6 +15,7 @@ export class TourDetailComponent implements OnInit {
   id: string;
   tour: Tour;
   cities: string[];
+  userProfile;
 
   constructor(private route: ActivatedRoute,
               private tourService: TourService,
@@ -43,10 +44,34 @@ export class TourDetailComponent implements OnInit {
             );
         }
       );
+    this.authService.getProfile()
+    .subscribe(
+      (data:any) => {
+        this.userProfile = data.user;
+        console.log(this.userProfile);
+      },
+      (error: any) => {
+        console.log("Error occured : " + error);
+      }
+    );
   }
 
   addBookmark() {
-
+    const user_id = this.userProfile._id;
+    const tour_id = this.tour._id;
+    const tour_title = this.tour.title;
+    const tour_duration = this.tour.duration;
+    const tour_price_adult = this.tour.price_adult;
+    const tour_price_child = this.tour.price_child;
+    const obj = {
+      "user_id" : user_id,
+      "tour_id" : tour_id,
+      "tour_title" : tour_title,
+      "tour_duration" : tour_duration,
+      "tour_price_adult" : tour_price_adult,
+      "tour_price_child" : tour_price_child
+    }
+    console.log(obj);
   }
 
 }
