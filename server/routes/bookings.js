8e6495daad3b7f876210de7e9f32424a63744ab1
tharindukumarasiri <ku.tharindu@gmail.com>
2019-06-03 21:02:35 +0530

    added bookings on server

---
+++ server/routes/bookings.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+const Booking = require('../models/booking');
+
+router.post('/', (req, res, next) => {
+    const newBooking = new Booking({
+        dp: req.body.dp,
+        adults: req.body.adults,
+        children: req.body.children
+    });
+    newBooking.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                status: 'Created Booking successfully',
+                createdMessage: {
+                    _id: result.id,
+                    dp: result.dp,
+                    adults: result.adults,
+                    children: result.children
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+module.exports = router;
