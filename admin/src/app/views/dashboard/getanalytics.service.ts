d4cd6295a0951b257cd72517776b5b8f7f30ae04
marioperera <pereramario19@gmail.com>
2019-06-03 20:05:09 +0530

    getanalytics modified

--- admin/src/app/views/dashboard/getanalytics.service.ts
+++ admin/src/app/views/dashboard/getanalytics.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Router } from "@angular/router";
+import { resolve } from "dns";
 
 @Injectable({
   providedIn: "root"
@@ -9,29 +10,34 @@ export class GetanalyticsService {
   public analyticsUrl: string = "http://localhost:3000/pageviews/Get_Analytics";
   public data: Array<number> = [];
   constructor(private http: HttpClient, private router: Router) {}
-  getChartData(bodyObject: Object) {
+  async getChartData(bodyObject: Object) {
     if (localStorage.getItem("token")) {
       // const headers = new HttpHeaders();
       // headers.append("token", localStorage.getItem("token"));
 
-      this.http
-        .post(this.analyticsUrl, {
-          bodyObject,
-          params: new HttpParams().append(
-            "token",
-            localStorage.getItem("token")
-          )
-        })
-        .subscribe(res => {
-          //console.log(res);
-          this.data = res["result"];
-          res["result"].array.forEach(element => this.data.push(element));
-          //console.log(this.data);
-          console.log(this.data);
-          return this.data;
-        });
+      await new Promise(resolve => {
+        this.http
+          .post(this.analyticsUrl, {
+            bodyObject,
+            params: new HttpParams().append(
+              "token",
+              localStorage.getItem("token")
+            )
+          })
+          .subscribe(res => {
+            //console.log(res);
+            this.data = res["result"];
+            res["result"].array.forEach(element => this.data.push(element));
+            //console.log(this.data);
+            console.log(this.data);
+            resolve();
+          });
+      }).then(val => {
+        return this.data;
+      });
 
-      //console.log(data);
+      console.log("line 33 get");
+      console.log(this.data);
       // var analytics: any = data.result;
       //console.log(data);
     } else {
