4ed9327e993020a4a61320e7914277ecc1fea890
tharindukumarasiri <ku.tharindu@gmail.com>
2019-06-19 08:46:14 +0530

    added payment gateway component.ts

--- client/src/app/pages/bookings/bookings-content/bookings-content.component.ts
+++ client/src/app/pages/bookings/bookings-content/bookings-content.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, AfterViewChecked  } from "@angular/core";
 
 import { AuthService } from "src/app/services/auth/auth.service";
 import { TourService } from "src/app/services/tour/tour.service";
 import { Tour } from "src/app/models/tour.model";
+
+declare let paypal: any;
 
 @Component({
   selector: "app-bookings-content",
   templateUrl: "./bookings-content.component.html",
   styleUrls: ["./bookings-content.component.css"]
 })
-export class BookingsContentComponent implements OnInit {
+export class BookingsContentComponent implements AfterViewChecked {
+
+  addScript: boolean = false;
+  paypalLoad: boolean = true;
+
+  finalAmount: number = 0;
+  num:number = 0;
+
   userProfile;
   bookings: Tour[];
   id: string;
@@ -28,6 +37,10 @@ export class BookingsContentComponent implements OnInit {
           (data: any) => {
             this.bookings = data.bookings;
             console.log(this.bookings);
+            for(this.num; this.num<this.bookings.length; this.num++){
+              console.log(this.bookings[this.num].price);
+              this.finalAmount= this.finalAmount + this.bookings[this.num].price; 
+            }
           },
           (error: any) => {
             console.log("Error occured : " + error);
@@ -38,6 +51,7 @@ export class BookingsContentComponent implements OnInit {
         console.log("Error occured : " + error);
       }
     );
+
   }
 
   removeBookings(index: number) {
@@ -52,4 +66,47 @@ export class BookingsContentComponent implements OnInit {
     );
     this.ngOnInit();
   }
+
+  paypalConfig = {
+    env: 'sandbox',
+    client: {
+      sandbox: 'Ac75lPF56Zzg5DYdWhKBcbc1LZg4ugmh40QTOhjJq1KLfIPZwKqJ4UkrX0HyfJs2ujUUR9KzAJSCieDs',
+      production: '<your-production-key here>'
+    },
+    commit: true,
+    payment: (data, actions) => {
+      return actions.payment.create({
+        payment: {
+          transactions: [
+            { amount: { total: this.finalAmount, currency: 'USD' } }
+          ]
+        }
+      });
+    },
+    onAuthorize: (data, actions) => {
+      return actions.payment.execute().then((payment) => {
+        //Do something when payment is successful.
+      })
+    }
+  };
+ 
+  ngAfterViewChecked(): void {
+    if (!this.addScript) {
+      this.addPaypalScript().then(() => {
+        paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
+        this.paypalLoad = false;
+      })
+    }
+  }
+  
+  addPaypalScript() {
+    this.addScript = true;
+    return new Promise((resolve, reject) => {
+      let scripttagElement = document.createElement('script');    
+      scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scripttagElement.onload = resolve;
+      document.body.appendChild(scripttagElement);
+    })
+  }
+
 }
